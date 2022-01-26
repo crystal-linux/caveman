84c010eb426771885c7216dbf0096a916dccd52c
Matt C <mdc028@bucknell.edu>
2022-01-25 23:20:00 -0500

    add miku_distrohop

--- bot.py
+++ bot.py
@@ -7,6 +7,19 @@ bot = commands.Bot(";")
 bot.add_cog(Packages(bot))
 
 
+@bot.command()
+async def distrohop(self, ctx, *, count=0):
+    """Count another miku distrohop"""
+    if os.path.exists("miku_distrohops.txt"):
+        with open("miku_distrohops.txt", "r") as f:
+            distrohops = int(f.read())
+    else:
+        distrohops = 0
+    distrohops += count
+    with open("miku_distrohops.txt", "w") as f:
+        f.write(str(distrohops))
+    await ctx.send("Miku distrohops: " + str(distrohops))
+
 if not os.path.exists(os.environ["HOME"] + "/.cavetoken"):
     print("No token found")
     exit()
